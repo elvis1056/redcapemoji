37467bc5d4dcfdfc621d931dc8af14119f44cdc0
elvis lin <john800116@hitmail.com>
2022-02-10 13:33:57 +0800

    [feat] 1. install fortawesome sass 2. install autoprefixer postcss tailwindcss in dev 3. setting tailwind.config.js 4. Entry Page and test component 5. add background img and headshot

--- src/layouts/Main/Navbar.js
+++ src/layouts/Main/Navbar.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const [stickyClass, setStickyClass] = useState('relative');
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      const windowHeight = window.scrollY;
+      windowHeight > 100 ? setStickyClass('fixed top-0 left-0 z-50') : setStickyClass('relative');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', stickNavbar);
+    return () => {
+      window.removeEventListener('scroll', stickNavbar);
+    };
+  }, []);
+
   return (
-    <div>
-      Navaaa
-    </div>
+    <>
+      <nav
+        className={`w-full bg-primary-default flex flex-wrap items-center justify-between px-2 py-3 z-9999 ${stickyClass}`}
+        // style={{ maxHeight: '50px' }}
+      >
+        <div className="w-full px-4 mx-auto flex flex-wrap items-center justify-between max-h-screen-70 overflow-y-auto">
+          <div className="w-full relative flex items-center justify-between lg:w-auto lg:static lg:block lg:justify-start text-white">
+            Elvis
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+              type="button"
+            >
+              <i className={`text-white ${navbarOpen ? 'fas fa-times animate-bounce ' : 'fas fa-bars'}`} />
+            </button>
+          </div>
+          <div
+            className={
+              'lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none'
+              + (navbarOpen ? ' block rounded bg-primary-light mt-4 lg:shadow-lg' : ' hidden')
+            }
+          >
+            <ul className="flex flex-col lg:flex-row list-none mr-auto lg:items-center" />
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto lg:items-center">
+              <li>
+                <a
+                  className="text-blueGray-800 hover:text-blueGray-600 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="https://www.facebook.com/profile.php?id=100000249132965"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <i className="text-blueGray-500 lg:text-white fab fa-facebook text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Share</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-blueGray-800 hover:text-blueGray-600 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="#pablo"
+                >
+                  <i className="text-blueGray-500  lg:text-white fab fa-github text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Star</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* <h1 className="text-3xl font-bold underline bg-slate-500">
+        Elvis
+      </h1> */}
+    </>
   );
 }
 
--- src/views/Entry/index.js
+++ src/views/Entry/index.js
@@ -1,8 +1,104 @@
 import React from 'react';
 
+// import Test from './components/test';
+import indexStyle from './index.module.scss';
+
 const Entry = () => {
   return (
-    <>12sss3aqdqdqsas</>
+    <main style={{ height: '60vh' }}>
+      <section
+        className={`${indexStyle.container}`}
+        style={{ position: 'relative' }}
+      >
+        <div className={`${indexStyle.bg} fixed`} />
+        <div className={`${indexStyle.section_wrapper} flex flex-row`}>
+          <div className="basis-1/12" />
+          <div
+            className={`basis-10/12 text-white flex flex-col pt-0 pb-0 ${indexStyle.main} md:flex-row md:pt-12 md:pb-8`}
+            // style={{ height: '100%' }}
+          >
+            <div className={`basis-5/12 p-12 ${indexStyle.left}`}>
+              <div className={`flex flex-col grow ${indexStyle.description}`}>
+                <h1>Elvis Lin 大鈞</h1>
+                <h4>A bit about Me</h4>
+                <ul className={'mt-4'}>
+                  <li>
+                    <ul className={`${indexStyle.description_text_block}`}>
+                      <li><b>NAME</b></li>
+                      <li>{'I\'M ELVIS LIN'}</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <ul className={`${indexStyle.description_text_block}`}>
+                      <li><b>PHONE </b></li>
+                      <li>+886 975 371 151</li>
+                    </ul>
+                  </li>
+                  <li>
+                    <ul className={`${indexStyle.description_text_block}`}>
+                      <li><b>E-MAIL </b></li>
+                      <li><a href="mailto:john800116@gmail.com">john800116@gmail.com</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className={`basis-7/12 p-4 ${indexStyle.right} md:p-12`}>
+              <div className={'flex grow'}>
+                <div>
+                  <img src={'/assets/img/bg-macao.jpeg'} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="basis-1/12" />
+          {/* <img
+            className="w-full"
+            src="/assets/img/bg-home-cafe.jpeg"
+            style={{ filter: 'blur(4px)', position: 'absolute', top: 0 }}
+          /> */}
+        </div>
+      </section>
+      {/* <Test /> */}
+      {/* <Test /> */}
+      {/* <Test /> */}
+      <section className={`${indexStyle.social_about_container}`}>
+        <div className={`${indexStyle.social_about_wrapper} flex flex-row`}>
+          <div className="basis-1/2">
+            <img className={`${indexStyle.head}`} />
+          </div>
+          <div className="basis-1/2" />
+        </div>
+      </section>
+      <section className={`${indexStyle.social_container}`}>
+        <ul className={`${indexStyle.social_icons}`}>
+          <li>
+            <a
+              className={'fab fa-google'}
+              href="#"
+            >
+              {/* <div className={'fab fa-google'} /> */}
+            </a>
+          </li>
+          <li>
+            <a
+              className={'fab fa-github'}
+              href="#"
+            >
+              {/* <div className={'fab fa-google'} /> */}
+            </a>
+          </li>
+          <li>
+            <a
+              className={'fab fa-facebook-square'}
+              href="#"
+            >
+              {/* <div className={'fab fa-google'} /> */}
+            </a>
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 };
 
