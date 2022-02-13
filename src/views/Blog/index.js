082f965694372a224fd5cfad6e46e4a5c909e866
elvis lin <john800116@hitmail.com>
2022-02-13 22:57:36 +0800

    [feat] Blog page construction

--- src/views/Blog/index.js
+++ src/views/Blog/index.js
@@ -16,6 +16,17 @@ const Blog = () => {
     })
   }
 
+  const articles = [
+    {
+      title: 'for...in vs for...of',
+      tags: ['JavaScript', 'for']
+    },
+    {
+      title: 'for...in vs for...of',
+      tags: ['JavaScript', 'for']
+    }
+  ]
+
   useEffect(() => {
     window.addEventListener('resize', setDimension);
 
@@ -28,14 +39,51 @@ const Blog = () => {
     <>
       <section className={`${indexStyle.container}`}>
         <div className={`${indexStyle.section_wrapper} flex flex-row`}>
-          <div className="basis-1/12" />
-          <div className="basis-10/12 flex flex-row">
-            <div className={`fixed bottom-20 md:static md:basis-3/12 p-4 ${indexStyle.left}`}>
-              {screenSize.dynamicWidth < 786 ? <i className={'fa fa-list'} /> : '索引'}
+          <div className="sm:basis-1/12" />
+          <div className="basis-full sm:basis-10/12 flex flex-row">
+            <div className={`fixed bottom-4 left-4 md:static md:basis-3/12 ${indexStyle.left}`}>
+              <div className={`p-4 pt-12 ${screenSize.dynamicWidth < 768 && 'border-solid border rounded-full pt-4'}`}>
+                {screenSize.dynamicWidth < 768 ? <i className={'fa fa-list'} /> : '文章分類'}
+              </div>
             </div>
-            <div className={`grow md:basis-9/12 p-4 ${indexStyle.left} w-full md:w-1/3`}>文章</div>
+            <div className={`grow md:basis-9/12 ${indexStyle.left} w-full md:w-1/3`}>
+              <div className="p-4 pt-6">
+                {
+                  articles.map(({ title, tags }) => {
+                    return (
+                      <div
+                        className="lg:pl-0 px-4 py-4 first:pt-0 flex-auto"
+                        key={title}
+                      >
+                        <div className="rounded shadow-lg hover:shadow-2xl transition-all  border-transparent">
+                          <div className="px-6 pt-4 text-base md:text-xl">{title}</div>
+                          <div className="px-6 pt-4 pb-2">
+                            {
+                              tags.map(tag => (
+                                <span
+                                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                                  key={tag}
+                                >
+                                  {tag}
+                                </span>
+                                // <button
+                                //   className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                                //   key={tag}
+                                // >
+                                //   {tag}
+                                // </button>
+                              ))
+                            }
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
           </div>
-          <div className="basis-1/12" />
+          <div className="sm:basis-1/12" />
         </div>
       </section>
       <Footer />
